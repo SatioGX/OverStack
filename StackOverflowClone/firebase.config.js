026075e82f3bc36a1baf4d4fe8ee28d6538ca3e2
PhucMaii <maithienphuc0102@gmail.com>
2023-10-21 12:58:16 -0700

    Refs #10: Firebase Set Up

---
+++ StackOverflowClone/firebase.config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_ID,
+  appId: import.meta.env.VITE_APP_APP_ID
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const googleProvider = new GoogleAuthProvider();
+const db = getFirestore();
+const storage = getStorage();
+
+export { auth, googleProvider, db, storage }
