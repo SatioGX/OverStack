bfb36b063369845d7d664ceedfd283cd1cb5e21e
Rangathan <121978255+Rangathan@users.noreply.github.com>
2023-10-23 21:18:02 -0700

    prabs way

--- StackOverflowClone/src/App.jsx
+++ StackOverflowClone/src/App.jsx
@@ -15,24 +15,47 @@ import Settings from './Pages/Settings/Settings';
 
 function App() {
 
-  
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [currentUserState, setCurrentUser] = useState();
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    setCurrentUser(currentUser);
+
+    if (!currentUser || location.pathname === "/signup") {
+      navigate("/signup");
+    }
+
+    if (!currentUser) {
+      navigate("/");
+    }
+
+    if (
+      currentUser &&
+      (location.pathname === "/" || location.pathname === "/signup")
+    ) {
+      navigate("/home");
+    }
+  }, []);
+
 
 
   return (
 
 
     <Routes>
+      {currentUserState && <Navbar data={currentUserState} />}
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path='/resetpassword' element={<PasswordReset />} />
-      { <Route path="/profile" element={
-       <RequireAuth> 
-         <Route path='/home' element={<HomePage />} />
+
+
+      <Route path='/home' element={<HomePage />} />
       <Route path='/createpost' element={<CreatePost />} />
       <Route path='/Settings' element={<Settings />} />
-       </RequireAuth>}
-      /> }
-      
+
+
+
     </Routes>
 
 
