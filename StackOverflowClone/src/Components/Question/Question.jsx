a66f3d208207839c8e9a8e76f9f32cf09447b1f6
Thien Phuc Mai <000461754@student.vcc.ca>
2023-11-16 13:04:49 -0800

    Refs#20: Finish Question Page UI

--- StackOverflowClone/src/Components/Question/Question.jsx
+++ StackOverflowClone/src/Components/Question/Question.jsx
@@ -4,23 +4,23 @@ import './style.css'
 
 export default function Question() {
   return (
-    <div className="grid p-2">
-        <div className="col-1 flex flex-column p-1 gap-4">
-            <h6 className='text-sm m-0'>0 votes</h6>
-            <h6 className='text-sm m-0'>0 answers</h6>
+    <div className="grid p-0">
+        <div className="col-1 flex flex-column p-2 gap-4 align-items-start">
+            <h6 className="text-sm m-0">0 votes</h6>
+            <h6 className="text-sm m-0">0 answers</h6>
         </div>
-        <div className="col-11 flex flex-column p-1 gap-4 align-items-start">
+        <div className="col-11 flex flex-column p-2 gap-4 align-items-start ">
             <h4 className="text-left m-0">How to make a table in reactjs</h4>
             <p className="text-sm m-0">i had ESM on MakerDao on april 2023 and i need to know how to access my vault information. i recieved mip65 dashboard and a vault loan but i dont know the usage. my admin account genera...</p>
         </div>
-        <div className='col-1'></div>
-        <div className="col-11 grid p-0 m-0">
+        <div className="col-1"></div>
+        <div className="col-11 grid px-2 py-0 m-0">
             <div className="col-8 flex p-0 m-0 gap-2 justify-content-start">
                 <Tag severity="info" value="Reactjs"></Tag>
                 <Tag severity="info" value="HTML"></Tag>
                 <Tag severity="info" value="CSS"></Tag>
             </div>
-            <div className="col-4 flex py-0 m-0 gap-2">
+            <div className="col-4 flex py-0 m-0 gap-2 justify-content-end">
                 <img src="https://cdn.pixabay.com/photo/2018/09/14/23/28/avatar-3678347_1280.png" className="avatar w-1rem" />
                 <h6 className="text-sm font-bold">Bin Mai</h6>
                 <h6 className="text-sm">asked 6 hours ago</h6>
--- StackOverflowClone/src/Pages/Questions/QuestionsPage.jsx
+++ StackOverflowClone/src/Pages/Questions/QuestionsPage.jsx
@@ -1,12 +1,37 @@
-import "./QuestionsPage.css"
-import { Divider } from "primereact/divider"
-import Question from "../../Components/Question/Question";
+import { Button } from 'primereact/button' 
+import { Divider } from 'primereact/divider'
+import Question from '../../Components/Question/Question';
 
 export default function QuestionsPage() {
   return (
-    <div className="flex-container">
-      <h2 className="flex-initial">All Questions</h2>
-      <div className="questions-container">
+    <div className="flex flex-column gap-2 px-8 mt-2 align-items-start">
+      <div className="flex w-full justify-content-between px-8 py-0">
+        <h2 className="text-xl font-bold">All Questions</h2>
+        <Button className="w-auto" severity="info">Ask Question +</Button>
+      </div>
+      <div className="flex w-full m-0 px-8 py-0">
+        <p className="text-lg w-full font-normal">2000 questions</p>
+        <div className="flex w-full justify-content-end m-0 p-0 p-buttonset">
+          <Button text raised className="gap-1">
+            <i 
+              className="pi pi-clock" 
+              style={{fontSize: "1rem", backgroundColor: "var(--primary-color)", color: "var(--highlight-bg)", borderRadius: '50%' }} 
+            /> 
+            Newest
+          </Button>
+          <Button text raised className="gap-1">
+            <i 
+              className="pi pi-question-circle" 
+              style={{fontSize: "1rem", backgroundColor: "var(--primary-color)", color: "var(--highlight-bg)", borderRadius: '50%'}} 
+            /> 
+            Unanswered
+          </Button>
+          <Button text raised className="gap-1"><i className="pi pi-thumbs-up-fill" style={{fontSize: "1rem"}} /> Most votes</Button>
+          <Button text raised className="gap-1"><i className="pi pi-thumbs-down-fill" style={{fontSize: "1rem"}} /> Least votes</Button>
+        </div>
+      </div>
+
+      <div className="flex flex-column w-full align-items-center px-8 py-0 m-0">
         <Divider className="m-0" />
           <Question />
         <Divider className="m-0" />
