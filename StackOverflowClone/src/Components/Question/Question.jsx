8445f9cface18d992a13149288db80727e9c1aa5
PhucMaii <maithienphuc0102@gmail.com>
2023-11-18 11:32:29 -0800

    Refs #20: Link to Firebase

--- StackOverflowClone/src/Components/Question/Question.jsx
+++ StackOverflowClone/src/Components/Question/Question.jsx
@@ -1,31 +1,65 @@
-import { Tag } from 'primereact/tag';        
+import PropTypes from 'prop-types'
+import { Tag } from 'primereact/tag'
 import './style.css'
-        
+import moment from 'moment/moment'
+import { collection, getDocs, query, where } from '@firebase/firestore';
+import { auth, db } from '../../../firebase.config';
+import { useEffect, useState } from 'react';
 
-export default function Question() {
+export default function Question({ questionData }) {
+    const [authorName, setAuthorName] = useState('');
+    let date = moment(questionData.createdAt.toDate());
+    const usersCollection = collection(db, 'users');
+    const userQuery = query(usersCollection, where('id', '==', auth.currentUser.uid));
+
+    useEffect(() => {
+        fetchAuthorInfo();
+    }, [])
+
+    const fetchAuthorInfo = async () => {
+        try {
+            const userSnapshot = await getDocs(userQuery);
+            userSnapshot.forEach(async (user) => {
+                const data = user.data();
+                setAuthorName(data.userName);
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
   return (
-    <div className="grid p-0">
-        <div className="col-1 flex flex-column p-2 gap-4 align-items-start">
+    <div className="grid w-full p-0 m-0">
+        <div className="col-1 flex flex-column p-0 m-0 gap-4 align-items-start">
             <h6 className="text-sm m-0">0 votes</h6>
             <h6 className="text-sm m-0">0 answers</h6>
         </div>
-        <div className="col-11 flex flex-column p-2 gap-4 align-items-start ">
-            <h4 className="text-left m-0">How to make a table in reactjs</h4>
-            <p className="text-sm m-0">i had ESM on MakerDao on april 2023 and i need to know how to access my vault information. i recieved mip65 dashboard and a vault loan but i dont know the usage. my admin account genera...</p>
+        <div className="col-11 flex flex-column p-0 m-0 gap-4 align-items-start">
+            <h4 className="text-left font-bold m-0 p-0">{questionData.title}</h4>
+            <p dangerouslySetInnerHTML={{__html: `${questionData.description.slice(0, 150)}`}} className="text-sm m-0"/>
         </div>
-        <div className="col-1"></div>
+        <div className="col-1 p-0 m-0"></div>
         <div className="col-11 grid px-2 py-0 m-0">
             <div className="col-8 flex p-0 m-0 gap-2 justify-content-start">
-                <Tag severity="info" value="Reactjs"></Tag>
-                <Tag severity="info" value="HTML"></Tag>
-                <Tag severity="info" value="CSS"></Tag>
+                {
+                    questionData.tags && questionData.tags.map((tag, index) => {
+                        return <Tag 
+                                    key={index} 
+                                    severity="info" 
+                                    value={tag}
+                                />
+                    })
+                }
             </div>
-            <div className="col-4 flex py-0 m-0 gap-2 justify-content-end">
+            <div className="col-4 flex p-0 m-0 gap-2 justify-content-end">
                 <img src="https://cdn.pixabay.com/photo/2018/09/14/23/28/avatar-3678347_1280.png" className="avatar w-1rem" />
-                <h6 className="text-sm font-bold">Bin Mai</h6>
-                <h6 className="text-sm">asked 6 hours ago</h6>
+                <h6 className="text-sm font-bold">{authorName}</h6>
+                <h6 className="text-sm">asked {date.fromNow()}</h6>
             </div>
         </div>
     </div>
   )
+}
+
+Question.propTypes = {
+    questionData: PropTypes.object.isRequired
 }
--- StackOverflowClone/src/Pages/Questions/QuestionsPage.jsx
+++ StackOverflowClone/src/Pages/Questions/QuestionsPage.jsx
@@ -1,39 +1,131 @@
 import { Button } from 'primereact/button' 
 import { Divider } from 'primereact/divider'
 import Question from '../../Components/Question/Question';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { db } from '../../../firebase.config';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function QuestionsPage() {
+  const [currentFilterTab, setCurrentFilterTab] = useState('Newest');
+  const [allQuestions, setAllQuestions] = useState([]);
+  const navigate = useNavigate();
+  const questionsCollection = collection(db, 'posts');
+
+  const tabList = [
+    {
+      name: 'Newest',
+      icon: 'pi pi-clock icon',
+      style: {
+        backgroundColor: "var(--primary-color)", 
+        color: "var(--highlight-bg)", 
+        borderRadius: '50%' 
+      }
+    },
+    {
+      name: 'Unanswered',
+      icon: 'pi pi-question-circle icon icon',
+      style: {
+        backgroundColor: "var(--primary-color)", 
+        color: "var(--highlight-bg)", 
+        borderRadius: '50%' 
+      }
+    },
+    {
+      name: 'Popularity',
+      icon: 'pi pi-thumbs-up-fill icon'
+    },
+    {
+      name: 'Minority',
+      icon: 'pi pi-thumbs-down-fill icon'
+    },
+  ];
+
+  useEffect(() => {
+    switch(currentFilterTab) {
+      case 'Newest':
+        fetchNewestQuestion();
+        break;
+      case 'Unanswered':
+        fetchUnansweredQuestions();
+        break;
+    }
+  }, [currentFilterTab])
+
+  const fetchNewestQuestion = async () => {
+    try {
+      const newQuestions = [];
+      const questionQuery = query(questionsCollection, orderBy('createdAt', 'desc'))
+      const questionSnapshot = await getDocs(questionQuery);
+      questionSnapshot.docs.forEach((question) => {
+        const data = question.data();
+        newQuestions.push(data);
+      })
+      setAllQuestions(newQuestions);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  const fetchUnansweredQuestions = () => {
+    const unansweredQuestions = allQuestions.filter((question) => {
+      return !question.answer;
+    })
+    setAllQuestions(unansweredQuestions);
+  }
+
+  const switchFilterTab = (tabName) => {
+    setCurrentFilterTab(tabName)
+  }
+  
   return (
     <div className="flex flex-column gap-2 px-8 mt-2 align-items-start">
       <div className="flex w-full justify-content-between px-8 py-0">
         <h2 className="text-xl font-bold">All Questions</h2>
-        <Button className="w-auto" severity="info">Ask Question +</Button>
+        <Button 
+          className="w-auto" 
+          severity="info"
+          onClick={() => navigate('/createpost')}
+        >
+          Ask Question +
+        </Button>
       </div>
       <div className="flex w-full m-0 px-8 py-0">
         <p className="text-lg w-full font-normal">2000 questions</p>
         <div className="flex w-full justify-content-end m-0 p-0 p-buttonset">
-          <Button text raised className="gap-1">
-            <i 
-              className="pi pi-clock" 
-              style={{fontSize: "1rem", backgroundColor: "var(--primary-color)", color: "var(--highlight-bg)", borderRadius: '50%' }} 
-            /> 
-            Newest
-          </Button>
-          <Button text raised className="gap-1">
-            <i 
-              className="pi pi-question-circle" 
-              style={{fontSize: "1rem", backgroundColor: "var(--primary-color)", color: "var(--highlight-bg)", borderRadius: '50%'}} 
-            /> 
-            Unanswered
-          </Button>
-          <Button text raised className="gap-1"><i className="pi pi-thumbs-up-fill" style={{fontSize: "1rem"}} /> Most votes</Button>
-          <Button text raised className="gap-1"><i className="pi pi-thumbs-down-fill" style={{fontSize: "1rem"}} /> Least votes</Button>
+          {
+            tabList && tabList.map((tab, index) => {
+              return (
+                <Button 
+                  onClick={() => switchFilterTab(tab.name)} 
+                  key={index} 
+                  text 
+                  raised 
+                  style={currentFilterTab === tab.name ? {backgroundColor: "var(--primary-color", color: "white"} : ''}
+                  className='gap-1'
+                >
+                  <i 
+                    className={tab.icon} 
+                    style={tab.style && tab.style}
+                  /> 
+                  {tab.name}
+                </Button>)
+            })
+          }
         </div>
       </div>
 
       <div className="flex flex-column w-full align-items-center px-8 py-0 m-0">
-        <Divider className="m-0" />
-          <Question />
+        {
+          allQuestions.length > 0 && allQuestions.map((question, index) => {
+            return (
+              <React.Fragment key={index}>
+                <Divider className="m-0" />
+                <Question questionData={question} />
+              </React.Fragment>
+            )
+          })
+        }
         <Divider className="m-0" />
       </div>
     </div>
