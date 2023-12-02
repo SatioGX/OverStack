0862d6b668e2d2d579c2cf440033a662d3c4e622
PhucMaii <maithienphuc0102@gmail.com>
2023-12-02 13:04:54 -0700

    Merge feat/search-function

--- StackOverflowClone/src/Pages/Settings/SettingsForm.jsx
+++ StackOverflowClone/src/Pages/Settings/SettingsForm.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
-import { Button } from 'primereact/button';
+import { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
-import { Checkbox } from 'primereact/checkbox';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import 'primereact/resources/primereact.css';
 
@@ -41,67 +39,58 @@ function SettingsForm() {
     />
   </div>
   </label>
-
-
-
         <div className = "flex-container">
-        <div className="flex-rectangle"></div>
-        <div className = "flex-col" > 
-            <label className = "flex-activity">Recent Activities</label>
+          <div className="flex-rectangle"></div>
+          <div className = "flex-col" > 
+              <label className = "flex-activity">Recent Activities</label>
+              <InputText
+              id="activity"
+              type="text"
+              placeholder="Recent activity here"
+              value={settingsData.activity}
+              onChange={(event) => setSettingsData({ ...settingsData, activity: event.target.value })}
+              className="read-only"
+            />
+          </div>
+        </div>
+        <div className = "flex-container">
+          <div className = "flex-panel margins">
+            <label >Bio:</label>
             <InputText
-            id="activity"
-            type="text"
-            placeholder="Recent activity here"
-            value={settingsData.activity}
-            onChange={(event) => setSettingsData({ ...settingsData, activity: event.target.value })}
-            className="read-only"
-          />
+              id="bio"
+              type="text"
+              placeholder="bio here"
+              value={settingsData.bio}
+              onChange={(event) => setSettingsData({ ...settingsData, bio: event.target.value })}
+              className="read-only"
+            />
+          </div>
+          <div className="flex-panel margins">
+            <label>Posts</label>
+              <InputText
+                id="posts"
+                type="text"
+                placeholder="posts here"
+                value={settingsData.bio}
+                onChange={(event) => setSettingsData({ ...settingsData, post: event.target.value })}
+                className="read-only"
+              />
+          </div>
+          <div className="flex-panel margins">
+            <label htmlFor="aboutMe">Achievements</label>
+            <InputText
+              id="achievements"
+              type="text"
+              placeholder="achievements here"
+              value={settingsData.bio}
+              onChange={(event) => setSettingsData({ ...settingsData, achievements: event.target.value })}
+              className="read-only"
+            />
+          </div>
         </div>
-        </div>
-
-
-        <div className = "flex-container">
-            <div className = "flex-panel margins">
-                <label >Bio:</label>
-                <InputText
-            id="bio"
-            type="text"
-            placeholder="bio here"
-            value={settingsData.bio}
-            onChange={(event) => setSettingsData({ ...settingsData, bio: event.target.value })}
-            className="read-only"
-          />
-            </div>
-
-            <div className = "flex-panel margins">
-                <label>Posts</label>
-                <InputText
-            id="posts"
-            type="text"
-            placeholder="posts here"
-            value={settingsData.bio}
-            onChange={(event) => setSettingsData({ ...settingsData, post: event.target.value })}
-            className="read-only"
-          />
-            </div>
-
-
-            <div className = "flex-panel margins">
-                <label htmlFor="aboutMe">Achievements</label>
-                <InputText
-            id="achievements"
-            type="text"
-            placeholder="achievements here"
-            value={settingsData.bio}
-            onChange={(event) => setSettingsData({ ...settingsData, achievements: event.target.value })}
-            className="read-only"
-          />
-            </div>
-        </div>
- 
         <div>
-            <label>Repos</label>
-            <InputText
+          <label>Repos</label>
+          <InputText
             id="repos"
             type="text"
             placeholder="repos here"
@@ -111,7 +100,7 @@ function SettingsForm() {
           />
         </div>
         <div>
-                <button className = "button">Save</button>
+          <button className="button">Save</button>
         </div>
     </form>
     </div>
